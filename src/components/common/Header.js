8f4007b6d23a880f52ffbe923a3248dadb253bca
HJ2828 <tjgmlwo0228@gmail.com>
2025-02-16 16:57:26 +0900

    feat: 헤더바/라우터 돔

---
+++ src/components/common/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className='header'>
+      <h1>LOGO</h1>
+      <nav>
+        <ul>
+          {/* <li><a href='/'>Home</a></li> */}
+        </ul>
+      </nav>
+    </header>
+  )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>뭔가 서비스 설명<br/>분석해보세요</h1>
+      <h2>저희는 안전하답니다...</h2>
+    </div>
+  )
+}
