a459a0bfdaf9ab41d1ed5d5c972f3d78afbc1a60
HJ2828 <tjgmlwo0228@gmail.com>
2025-02-18 23:06:00 +0900

    feat: theme 적용

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from "styled-components";
+import theme from './theme';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>
 );
