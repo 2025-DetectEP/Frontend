140d7719bfa3d57f86675187bf8aa0836904630f
HJ2828 <tjgmlwo0228@gmail.com>
2025-02-19 15:43:59 +0900

    feat: 폰트 추가

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from "styled-components";
 import theme from './theme';
 import App from './App';
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
