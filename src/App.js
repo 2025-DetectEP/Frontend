908c3358a8c03b25f8a13cf6927f047f385bf77c
HJ2828 <tjgmlwo0228@gmail.com>
2025-02-20 23:02:39 +0900

    feat: footer 기본 틀 UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Header from "./components/common/Header";
 import './App.css';
+import Footer from './components/common/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
         </Routes>
       </main>
+      <Footer />
     </div>
   );
 }
