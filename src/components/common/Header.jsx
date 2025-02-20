2f72ca85da46d0b6340b3dfa00341a8b4db0eacc
HJ2828 <tjgmlwo0228@gmail.com>
2025-02-20 22:57:04 +0900

    feat: 헤더 로그인 버튼 클릭 시 로그인 모달

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,24 +1,38 @@
 import React, { useState } from 'react';
 import styles from './Header.module.css';
+import LoginModal from '../Login/LoginModal';
 
 export default function Header() {
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);  // 로그인 여부
+  const [isModalOpen, setIsModalOpen] = useState(false);  // 로그인 모달 여부
+  
+  // 로그인 여부에 따라 모달(로그인X -> 로그인 모달)
+  const handleClick = () => {
+    if (isLogin) {  // 로그인O
+       setIsModalOpen(false);
+       console.log("로그인o")
+       // 다른 활동 작성 예정
+    } else {  // 로그인X
+      setIsModalOpen(true); // 로그인 모달 열기
+      console.log("로그인x")
+    }
+  }
 
   return (
     <header className={styles.header}>
       <div className={styles.headerObj}>
         <h1>LOGO</h1>
-        {/* <nav>
-          <ul>
-            <li><a href='/'>Home</a></li>
-          </ul>
-        </nav> */}
         {isLogin ? 
           <button className={styles.profile}>
           </button>
-          : <button className={styles.loginBtn}>로그인</button>
+          : <button className={styles.loginBtn} onClick={handleClick}>로그인</button>
         }
       </div>
+
+      {isModalOpen && (
+        <LoginModal setIsModalOpen={setIsModalOpen} />
+        )
+      }
     </header>
   )
 }
