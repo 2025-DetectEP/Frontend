b0aedb35dd32f7f745aa271733240947b9862314
HJ2828 <tjgmlwo0228@gmail.com>
2025-02-24 02:40:46 +0900

    feat: footer [PRBE-13]

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import styled from "styled-components";
 import '../../App.css';
 import media from '../../styles/media';
+import { fontSizes } from '../../styles/FontSizes';
 
 export default function Footer() {
   return (
     <FooterContainer>
-      
+      <TermsAndPrivacyContainer>
+        <span>필수 약관</span>
+        <span>개인정보처리방침</span>
+      </TermsAndPrivacyContainer>
+      <h1>Pribee</h1>
+      <BrandingContainer>
+        <span>SNS 개인정보 탐지 보조 서비스<br/>2025.03</span>
+        <span>Team Olibees</span>
+      </BrandingContainer>
     </FooterContainer>
   );
 }
@@ -19,4 +28,19 @@ const FooterContainer = styled.div`
   align-items: flex-start;
   gap: 10px;
   background: ${(props) => props.theme.Secondary};
+`;
+
+const TermsAndPrivacyContainer = styled.div`
+  display: flex;
+  gap: 74px;
+  ${fontSizes.bdCaption2Light};
+  color: ${(props) => props.theme.Gray200};
+`;
+
+const BrandingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.222vh;
+  ${fontSizes.bdCaption2Light};
+  color: ${(props) => props.theme.Gray400};
 `;
