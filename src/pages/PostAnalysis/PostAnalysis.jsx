2cb31bfffa779d24e4a4463bb4ad09329efe0586
HJ2828 <tjgmlwo0228@gmail.com>
2025-02-23 21:02:30 +0900

    feat: 기본 게시물 분석 섹션1 UI [PRBE-13]

--- src/pages/PostAnalysis/PostAnalysis.jsx
+++ src/pages/PostAnalysis/PostAnalysis.jsx
@@ -1,11 +1,18 @@
 import React, { useContext, useEffect, useState } from 'react';
 import * as S from "./PostAnalysisStyled";
 import theme from '../../styles/theme';
+import PostAnalysisSection1 from '../../components/PostAnalysis/PostAnalysisSection1';
 
-export default function Home() {
+export default function PostAnalysis() {
   return (
-    <>
-    
-    </>
+    <S.Main>
+      <S.Section1>
+        <PostAnalysisSection1 />
+      </S.Section1>
+
+      <S.Section2>
+
+      </S.Section2>
+    </S.Main>
   );
 }
--- src/pages/PostAnalysis/PostAnalysisStyled.js
+++ src/pages/PostAnalysis/PostAnalysisStyled.js
@@ -2,3 +2,23 @@ import styled from "styled-components";
 import '../../App.css';
 import media from '../../styles/media';
 import { fontSizes } from "../../styles/FontSizes";
+
+export const Main = styled.div`
+
+`;
+
+export const Section1 = styled.div`
+  background-color: ${(props) => props.theme.Background_solid};
+    /* padding-top: 7.407vh; */
+    padding-left: 12.5vw;
+    padding-right: 12.5vw;
+    ${media.small`
+        /* padding-top: 5.556vh; */
+        padding-left: 3.962vw;
+        padding-right: 3.962vw;
+    `}
+`;
+
+export const Section2 = styled.div`
+
+`;
