54e6a87672ac181491ae8e76a1f706e83e91db18
HJ2828 <tjgmlwo0228@gmail.com>
2025-02-23 22:55:43 +0900

    style: 기존 게시글 섹션1 줄바꿈 수정 [PRBE-13]

--- src/components/PostAnalysis/PostAnalysisSection1.jsx
+++ src/components/PostAnalysis/PostAnalysisSection1.jsx
@@ -22,7 +22,9 @@ export default function PostAnalysisSection1({isPost, postCount, analysisCount, 
         <InfoContainer $count={analysisCount}>
           {isPost ? 
             <span>전체 게시물 {postCount}개 중<br />
-            개인정보가 포함된 게시물이 <span className='analysisCount'>{analysisCount}개</span> 발견되었어요.</span>
+            개인정보가 포함된 게시물이&nbsp;
+            <br className='lineBreak'/>
+            <span className='analysisCount'>{analysisCount}개</span> 발견되었어요.</span>
           : 
             <span>해당 계정에<br />게시물이 존재하지 않아요.</span>
           }
@@ -82,6 +84,13 @@ const AccountContainer = styled.div`
 const InfoContainer = styled.div`
   ${fontSizes.titleHead2bold};
   color: ${(props) => props.theme.Gray700};
+
+  .lineBreak {
+    display: none;
+    ${media.small`
+      display: block;
+    `}
+  }
 
   .analysisCount {
     color: ${(props) => props.$count > 0 ? props.theme.Warning : props.theme.Gray700};
