d691766544b356d3e9fb31f2ce68233da76d8add
HJ2828 <tjgmlwo0228@gmail.com>
2025-02-28 01:02:28 +0900

    feat: 게시물 이미지 분석 발견X [PRBE-13]

--- src/components/common/Analysis/ImageAnalysisInform.jsx
+++ src/components/common/Analysis/ImageAnalysisInform.jsx
@@ -5,21 +5,28 @@ import '../../../App.css';
 import { fontSizes } from "../../../styles/FontSizes";
 import Tag from './Tag';
 
-const ImageAnalysisInform = () => {
+const ImageAnalysisInform = ({isImgAnalysis}) => {
   return (
-    <Main>
-      <span className='title'>사진에서 발견된 정보</span>
-      <TagContainer>
-        <Tag />
-        <Tag />
-        <Tag />
-        <Tag />
-        <Tag />
-      </TagContainer>
-      <Content>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti eum maxime, cupiditate omnis reiciendis assumenda quibusdam sint iure laboriosam natus saepe dignissimos, illum perspiciatis cum, nihil nisi at quo. Voluptas.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti eum maxime, cupiditate omnis reiciendis assumenda quibusdam sint iure laboriosam natus saepe dignissimos, illum perspiciatis cum, nihil nisi at quo. Voluptas.
-      </Content>
+    <Main $isImgAnalysis={isImgAnalysis}>
+      {isImgAnalysis ?
+        <FindContainer>
+          <span className='title'>사진에서 발견된 정보</span>
+          <TagContainer>
+            <Tag />
+            <Tag />
+          </TagContainer>
+          <Content>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores temporibus voluptates omnis est! Perspiciatis soluta odit nemo asperiores totam iste delectus praesentium, atque, dolore dolor dolorem dolores reprehenderit cupiditate illum. 
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores temporibus voluptates omnis est! Perspiciatis soluta odit nemo asperiores totam iste delectus praesentium, atque, dolore dolor dolorem dolores reprehenderit cupiditate illum. 
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores temporibus voluptates omnis est! Perspiciatis soluta odit nemo asperiores totam iste delectus praesentium, atque, dolore dolor dolorem dolores reprehenderit cupiditate illum. 
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores temporibus voluptates omnis est! Perspiciatis soluta odit nemo asperiores totam iste delectus praesentium, atque, dolore dolor dolorem dolores reprehenderit cupiditate illum. 
+          </Content>
+        </FindContainer>
+      :
+        <NotFindContainer>
+          <span className='noTitle'>전체 사진에 개인정보가 발견되지 않았어요!</span>
+        </NotFindContainer>
+      }
     </Main>
   );
 }
@@ -28,28 +35,43 @@ export default ImageAnalysisInform;
 
 const Main = styled.div`
   z-index: 10;
-  display: flex;
-  flex-direction: column;
-  justify-content: start;
-  gap: 1.481vh;
   border-radius: 8px;
-  background: rgba(255, 255, 255, 0.90);
+  background: ${(props) => props.$isImgAnalysis ? 'rgba(255, 255, 255, 0.90)' : 'rgba(255, 249, 229, 0.90)'};
   box-shadow: 0px 0px 16px 0px #666868;
   position: absolute;
   left: 0;
   top: 0;
   margin: 2.963vh 1.667vw;
   padding: 2.963vh 1.667vw;
+  width: calc(100% - 1.667vw - 1.667vw);
 
   @media only screen and (max-width: 1024px) { //700px
-    margin: 2.667vw 1.481vh;
+    margin: 1.481vh 2.667vw;
     padding: 2.222vh 4vw;
+    width: calc(100% - 1.481vh - 1.481vh);
   }
   
   .title {
     ${fontSizes.titleHead3Bold};
     color: ${(props) => props.theme.Black};
   }
+
+  .noTitle {
+    ${fontSizes.body3Regular};
+    color: ${(props) => props.theme.Black};
+  }
+`;
+
+const FindContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: stretch;
+  gap: 1.481vh;
+`;
+
+const NotFindContainer = styled.div`
+
 `;
 
 const TagContainer = styled.div`
@@ -62,5 +84,6 @@ const Content = styled.div`
   ${fontSizes.body3Regular};
   color: ${(props) => props.theme.Black};
   max-height: 9.259vh;
+  min-height: 3vh;
   overflow-y: auto;
 `;
