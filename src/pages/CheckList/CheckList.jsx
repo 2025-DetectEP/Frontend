7fa5473eaf659b13fe50e84b95af4db8ba8a6dc6
HJ2828 <tjgmlwo0228@gmail.com>
2025-03-01 00:43:26 +0900

    feat: 체크리스트 초기 세팅, home과 연결 [PRBE-19]

---
+++ src/pages/CheckList/CheckList.jsx
@@ -0,0 +1,12 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as S from "./CheckListStyled";
+import theme from '../../styles/theme';
+import { useAuth } from '../../context/AuthContext';
+
+export default function Home() {
+
+  return (
+    <></>
+  );
+}
