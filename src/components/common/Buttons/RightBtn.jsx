c5f07d9737f569669f2618a7167426f1bc1d79f4
HJ2828 <tjgmlwo0228@gmail.com>
2025-03-04 22:20:58 +0900

    fix: 기존 게시물 필터 x-스크롤 [PRBE-22]

--- src/components/common/Buttons/RightBtn.jsx
+++ src/components/common/Buttons/RightBtn.jsx
@@ -1,11 +1,52 @@
-import React, { forwardRef, useState } from 'react';
+import React, { forwardRef, useEffect } from 'react';
 import styled from "styled-components";
 import '../../../App.css';
 import { ReactComponent as IconRightEnabled } from '../../../assets/icons/icon_right_enabled.svg'
 import { ReactComponent as IconRightHover } from '../../../assets/icons/icon_right_hover.svg'
 import { ReactComponent as IconRightPressed } from '../../../assets/icons/icon_right_pressed.svg'
 
-const RightBtn = forwardRef(({isHovered, isPressed}, ref) => {
+const RightBtn = forwardRef(({isHovered, isPressed, startScrolling, stopScrolling}, ref) => {
+  useEffect(() => {
+    const element = ref.current;
+    if (!element) return;
+
+    const handleMouseDown = () => {
+      startScrolling("right");
+    };
+
+    const handleMouseUp = () => {
+      stopScrolling();
+    };
+
+    const handleMouseLeave = () => {
+      stopScrolling();
+    };
+
+    element.addEventListener("mousedown", handleMouseDown, true);
+    element.addEventListener("mouseup", handleMouseUp, true);
+    element.addEventListener("mouseleave", handleMouseLeave, true);
+
+    return () => {
+      element.removeEventListener("mousedown", handleMouseDown, true);
+      element.removeEventListener("mouseup", handleMouseUp, true);
+      element.removeEventListener("mouseleave", handleMouseLeave, true);
+    };
+  }, [ref]);
+  // useEffect(() => {
+  //   const element = ref.current;
+  //   if (!element) return;
+
+  //   const handleMouseDown = () => {
+  //     startScrolling("right");
+  //   };
+
+  //   element.addEventListener("mousedown", handleMouseDown, true);
+
+  //   return () => {
+  //     element.removeEventListener("mousedown", handleMouseDown, true);
+  //   };
+  // }, [ref]);
+
   return (
     <Main ref={ref}>
       {isPressed ? (
